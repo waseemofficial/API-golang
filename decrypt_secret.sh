5a393c2e79ee84e9a5c6211efdb06893e78aa121
root <root@DESKTOP-RBA1JLD>
2023-09-22 01:07:53 +0530

    try this dectypt

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -4,5 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$Password" \
---output $HOME/secrets/my_secret.json my_secret.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$Password" --output $HOME/secrets/my_secret.json my_secret.json.gpg
