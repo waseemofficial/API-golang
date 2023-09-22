99007ad4142ac5b3254b301b6b67e71ea9793b2d
was <sy.waseemofficial@gmail.com>
2023-09-22 17:23:23 +0530

    $ADDED SHELL LINK5

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -5,7 +5,3 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$Password" --output $HOME/secrets/my_secret.json my_secret.json.gpg
-
-#export GOOGLE_APPLICATION_CREDENTIALS="$HOME/"
-export NAMES="ji sunta ho"
-echo -e "\e[32m $NAME \e[0m"
